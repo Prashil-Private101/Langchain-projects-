a9104ae5c0dec3387a6f4f58b9401f12d8baac0f
Prashil-Private101 <prashilthool4791@gmail.com>
2026-02-16 13:16:43 +0530

    Add initial chatbot implementation using ChatOpenAI

---
+++ Prompts/chatbot.py
@@ -0,0 +1,15 @@
+from langchain_openai import ChatOpenAI
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+while True:
+    user_input= input('You: ')
+
+    if user_input.lower() == 'exit':
+        break
+    result = model.invoke(user_input)
+    print('AI: ', result.content)
