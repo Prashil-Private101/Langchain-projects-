44754c60b4fbd032ebcd5176506d68863834a2ed
Prashil-Private101 <prashilthool4791@gmail.com>
2026-02-21 21:32:58 +0530

    Add structured output parser and prompt template for fact generation

---
+++ OutPutParsers/structured_output_parser.py
@@ -0,0 +1,42 @@
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+
+from dotenv import load_dotenv
+
+from langchain_core.prompts import PromptTemplate  
+
+from langchain_classic.output_parsers import ResponseSchema,StructuredOutputParser
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-20b",
+    task="text-generation",
+    #max_new_tokens=100,
+   # huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+schema= [
+    ResponseSchema(name= 'fact_1', description = 'Fact 1 about the topic'),
+    ResponseSchema(name= 'fact_2', description = 'Fact 2 about the topic'),
+    ResponseSchema(name= 'fact_3', description = 'Fact 3 about the topic'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template= 'Give 3 fact about {topic} \n {format_instruction}',
+    input_variables= ['topic'],
+    partial_variables={'format_instruction' : parser.get_format_instructions()}
+)
+
+prompt = template.format(topic='black hole')
+
+result = model.invoke(prompt)
+
+final_result = parser.parse(result.content)
+
+print(final_result)
