2bc2edbf2973e9778fb7b3a33240a3737fc96a20
Prashil-Private101 <prashilthool4791@gmail.com>
2026-03-17 13:59:14 +0530

    vectorStore Retrivers initial example

---
+++ Retrivers/vector_store_retrivers.py
@@ -0,0 +1,30 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from dotenv import load_dotenv
+
+load_dotenv()
+
+documents = [
+    Document(page_content="LangChain helps developers build LLM applications easily."),
+    Document(page_content="Chroma is a vector database optimized for LLM-based search."),
+    Document(page_content="Embeddings convert text into high-dimensional vectors."),
+    Document(page_content="OpenAI provides powerful embedding models."),
+]
+
+embedding_model = OpenAIEmbeddings()
+
+vector_store = Chroma.from_documents(
+    documents=documents,
+    embedding=embedding_model,
+    collection_name="my_collection"
+)
+
+retriver = vector_store.as_retriever(search_kwargs={"k":2})
+
+query = "what is Chroma used for"
+result  = retriver.invoke(query)
+
+for i , doc in enumerate(result):
+    print(f"\n-----Result{i+1}-----")
+    print(doc.page_content)
