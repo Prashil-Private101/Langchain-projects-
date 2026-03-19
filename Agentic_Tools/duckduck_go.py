8d2e00208dae7098310c6ae79ccd13fa712e32a9
Prashil-Private101 <prashilthool4791@gmail.com>
2026-03-19 13:34:09 +0530

    initial implementation of built in tool

---
+++ Agentic_Tools/duckduck_go.py
@@ -0,0 +1,9 @@
+from langchain_community.tools import DuckDuckGoSearchRun
+search_tool = DuckDuckGoSearchRun()
+result = search_tool.invoke('ipl news')
+print(result)
+
+from langchain_community.tools import ShellTool
+shell_tool = ShellTool()
+
+results = shell_tool.invoke('ls')
