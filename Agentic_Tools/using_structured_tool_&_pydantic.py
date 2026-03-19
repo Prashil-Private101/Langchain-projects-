b00747bcf9bf2750d5b5b0c1613a7edd078082f7
Prashil-Private101 <prashilthool4791@gmail.com>
2026-03-19 14:19:56 +0530

    initial implementaion of Structured Tool

---
+++ Agentic_Tools/using_structured_tool_&_pydantic.py
@@ -0,0 +1,24 @@
+from langchain_core.tools import StructuredTool
+from pydantic import BaseModel , Field
+
+class MultiplyInput(BaseModel):
+    a: int = Field(required = True, description="the first number to add")
+    b: int = Field(required=True, description="the second number to add")
+
+def multiply_func(a: int , b: int)-> int:
+    return a * b
+
+multiply_tool = StructuredTool.from_function(
+    func= multiply_func,
+    name = 'multiply',
+    description="multipy two integes",
+    args_schema= MultiplyInput
+)
+
+result = multiply_tool.invoke({"a":3,"b":5})
+print(result)
+print(multiply_tool.name)
+print(multiply_tool.description)
+print(multiply_tool.args)
+
+print(multiply_tool.args_schema.model_json_schema())
