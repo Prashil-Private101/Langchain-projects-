f341d4ff7ca94fdc0f0865f84fd018c86e4f4fb6
Prashil-Private101 <prashilthool4791@gmail.com>
2026-03-19 13:45:24 +0530

    Add DuckDuckGo search tool and implement custom multiply function

--- Agentic_Tools/duckduck_go.py
+++ Agentic_Tools/duckduck_go.py
@@ -2,7 +2,9 @@ from langchain_community.tools import DuckDuckGoSearchRun
 search_tool = DuckDuckGoSearchRun()
 result = search_tool.invoke('ipl news')
 print(result)
-
+print(search_tool.name)
+print(search_tool.description)
+print(search_tool.args)
 from langchain_community.tools import ShellTool
 shell_tool = ShellTool()
 
